6a495b0f2bd085868b9117310618a848d1c7f500
Oday Maleh <oday.maleh@gmail.com>
2016-02-24 15:12:06 +0200

    Generates legal XML file based on the yaml file that it was fed and places the new file in the current project

--- scaffolder.py
+++ scaffolder.py
@@ -1,57 +1,56 @@
 #!/usr/bin/env python3
 
-from yaml import safe_load as yaml_safe_load
+from yaml import load as yaml_load
 from lxml import etree as ET
-import xml_generator
-import stack
+
+NSMAP = {"android": "http://schemas.android.com/apk/res/android"}
+tagDimensions = {"layout_width": "wrap_content",
+                 "layout_height": "wrap_content"}
 
 
 def parse_yaml(file_path):
     with open(file_path, "r") as stream:
-        stream = yaml_safe_load(stream)
+        stream = yaml_load(stream)
         return stream
 
 
-def traverse(output, file, root_element, last_element):
-    if isinstance(output, list):
-        for item in output:
-            traverse(item, file, root_element, last_element)
-    elif isinstance(output, dict):
-        for key, value in output.items():
-            if isinstance(value, dict):
-                # element with attrs
-                sub_element = ET.SubElement(last_element, key)
-                for attr_name, attr_value in value.items():
-                    sub_element.set(attr_name, attr_value)
-            elif isinstance(value, list):
-                # layout element
-                last_element = ET.Element(key)
-                root_element.append(last_element)
+def traverse(node, parent_element):
+    try:
+        name = list(node.keys())[0]
+        sub_element = ET.SubElement(parent_element, name)
+    except AttributeError:
+        ET.SubElement(parent_element, node)
+    else:
+        for child in node.get(name, []):
+            if 'attrs' in child:
+                config = child['attrs']
 
-            traverse(output[key], file, root_element, last_element)
-
-    elif isinstance(output, str) and output in xml_generator.tagNames:
-        ET.SubElement(last_element, output)
-
-    return root_element
+                for key, value in config.items():
+                    sub_element.attrib[ET.QName(NSMAP['android'], key)] = value
+            else:
+                traverse(child, sub_element)
+    return parent_element
 
 
 def main(source, target):
     with open(target, "w") as target_file:
-        target_file.write(xml_generator.xml_header)
 
         yaml_output = parse_yaml(source)
 
         yaml_activity = yaml_output.get('activity')
 
-        root_element = ET.Element(next(iter(yaml_activity)))
+        top_element = ET.Element(next(iter(yaml_activity)), nsmap=NSMAP)
 
-        print(yaml_activity)
-        element_stack = stack.Stack()
+        tree = ET.ElementTree(traverse(yaml_activity, top_element))
 
-        tree = ET.ElementTree(traverse(yaml_activity, target_file, root_element, None))
-        tree.write("trial.xml", pretty_print=True)
+        for i, tag in enumerate(tree.getiterator()):
+            if i == 1:
+                tree._setroot(tag)
+            for key, value in tagDimensions.items():
+                tag.attrib[ET.QName(NSMAP['android'], key)] = value
+
+        tree.write(target_file.name, pretty_print=True, xml_declaration=True, encoding='utf-8')
 
 
 if __name__ == '__main__':
-    main("stream.yaml", "newfile.xml")
+    main("stream.yaml", "trial.xml")
